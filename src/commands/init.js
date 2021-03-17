8abfdbcbc8d873fcbc1ca0186c2510e2904b79d9
Justin <justws95@gmail.com>
2021-03-17 01:10:17 -0400

    Pushing changes from work today

--- src/commands/init.js
+++ src/commands/init.js
@@ -2,15 +2,20 @@ const {Command} = require('@oclif/command');
 const yaml = require('js-yaml');
 const fs = require('fs');
 const os = require('os');
-const prompt = require('prompt');
+const inquirer = require('inquirer');
 const figlet = require('figlet');
 const chalk = require('chalk');
-const colors = require('colors/safe');
-const endOfLine = require('os').EOL;
+// const endOfLine = require('os').EOL;
 const brunoUtils = require('./../common/utils');
 
 class InitCommand extends Command {
   async run() {
+    // Verify this is not an existing Bruno project
+    if (brunoUtils.is_bruno_repo()) {
+      console.log(chalk.redBright('Error: This is already a bruno tracked repository'));
+      return;
+    }
+
     // Display the welcome banner
     console.log(
       chalk.cyanBright.bold(
@@ -18,111 +23,107 @@ class InitCommand extends Command {
       )
     );
 
-    const path = './bruno.yml';
+    const BRUNO_PATH = './bruno.yml';
 
-    if (brunoUtils.is_bruno_repo()) {
-      console.log('Error: This is already a bruno tracked repository');
-      return;
-    }
-
-    let newProject = {
-      projectName: 'my-bruno-project',
+    /*
+      Create a new project using The Pitchfork Layout (PFL) for file structure. 
+      Reference: https://api.csswg.org/bikeshed/?force=1&url=https://raw.githubusercontent.com/vector-of-bool/pitchfork/develop/data/spec.bs
+    */
+    let BRUNO_PROJ = {
+      project_name: 'my-bruno-project',
       compiler: '',
-      buildSystem: 'make',
-      languageStandard: 'latest',
-      trackedFiles: [],
-      projectDependancies: {},
-      binaries: {},
+      build_system: 'make',
+      language_standard: 'latest',
+      project_structure: {
+        build_dir: 'build',
+        include_dir: 'include',
+        source_dir: 'src',
+        test_dir: 'tests',
+        examples_dir: 'examples',
+        external_dir: 'external',
+        data_dir: 'data',
+        tools_dir: 'tools',
+        documentation_dir: 'docs',
+        libs_dir: 'libs',
+        extras_dir: 'extras',
+      }
     };
 
     switch (os.platform()) {
     case 'linux':
-      newProject.compiler = 'gcc';
-      newProject.buildSystem = 'make';
+      BRUNO_PROJ.compiler = 'gcc';
+      BRUNO_PROJ.buildSystem = 'make';
       break;
     case 'darwin':
-      newProject.compiler = 'clang';
-      newProject.buildSystem = 'make';
+      BRUNO_PROJ.compiler = 'clang';
+      BRUNO_PROJ.buildSystem = 'make';
       break;
     case 'win32':
-      newProject.compiler = 'gcc';
-      newProject.buildSystem = 'make';
+      BRUNO_PROJ.compiler = 'gcc';
+      BRUNO_PROJ.buildSystem = 'make';
       break;
     default:
-      newProject.compiler = 'gcc';
-      newProject.buildSystem = 'make';
+      BRUNO_PROJ.compiler = 'gcc';
+      BRUNO_PROJ.buildSystem = 'make';
       break;
     }
 
-    const promptParams = [
+    const questions = [
       {
-        name: 'project_name',
-        description: 'project name: ',
-        type: 'string',
-        required: true,
-        validator: /^[a-zA-Z\s\-]+$/, // TODO: Change validator
-        warning: 'Project name must be only letters, spaces, or dashes.',
-        default: `${newProject.projectName}`,
+        name: 'project-name',
+        type: 'input',
+        default: BRUNO_PROJ.project_name,
+        message: 'Name of new Bruno project: ',
       },
       {
         name: 'compiler',
-        description: 'compiler: ',
-        type: 'string',
-        required: true,
-        default: `${newProject.compiler}`,
+        type: 'input',
+        default: BRUNO_PROJ.compiler,
+        message: 'Compiler: ',
       },
       {
-        name: 'build_system',
-        description: 'build system: ',
-        type: 'string',
-        required: true,
-        default: `${newProject.buildSystem}`,
+        name: 'build-system',
+        type: 'input',
+        default: BRUNO_PROJ.build_system,
+        message: 'Build System: ',
       },
       {
-        name: 'language_standard',
-        description: 'language standard: ',
-        type: 'string',
-        required: true,
-        default: `${newProject.languageStandard}`,
+        name: 'lang-std',
+        type: 'list',
+        choices: [
+          'Latest',
+          'C++20/C++2a',
+          'C++17/C++1z',
+          'C++14/C++1y',
+          'C++11/C++0x',
+          'C++03',
+          'C++98',
+        ],
+        default: BRUNO_PROJ.language_standard,
+        message: 'Language Standard: ',
       },
       {
-        name: 'isOk',
-        description: (endOfLine + `${newProject.toString()}` +  endOfLine + 'Is this ok?:'), //TODO: Doesnt actually print obj
-        type: 'string',
-        required: true,
-        validator: /^[a-zA-Z\s\-]+$/, // TODO: Change validator
-        warning: 'Only yes or no answers will be accepted.',
-        default: 'yes',
-      }
+        name: 'pitchfork',
+        type: 'confirm',
+        message: `Use The Pitchfork Layout (PFL) project structure? ${chalk.yellow('Recomended for larger projects.')}`,
+      },
     ];
 
-    prompt.message = colors.green('> ');
-    prompt.delimiter = '';
-    prompt.start();
-
-    prompt.get(promptParams, function (err, result) {
-      if (err) {
-        console.log(err);
-        return;
+    inquirer.prompt(questions)
+    .then(answers => {
+      if (answers.p)
+    })
+    .catch(error => {
+      if(error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
       }
-
-      // TODO: Shorten if possible
-      if (result.isOk === 'n' || result.isOk === 'no' || result.isOk === 'N' || result.isOk === 'No') {
-        console.log(colors.red('Aborted'));
-        return;
-      }
-
-      for (const [key, value] of Object.entries(result)) {
-        if (key === 'isOk') {
-          continue;
-        }
-
-        newProject[key] = value;
-      }
-
-      let yamlStr = yaml.safeDump(newProject);
+    })
+    .finally(answers => {
+      let yamlStr = yaml.safeDump(BRUNO_PROJ);
       fs.writeFileSync('./bruno.yml', yamlStr, 'utf8');
-      console.log(colors.green(`Initialized new Bruno repository in ${path}`));
+      console.log(chalk.magenta(`Initialize new Bruno project at ${BRUNO_PATH}`));
     });
   }
 }
