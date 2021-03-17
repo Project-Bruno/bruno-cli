9848a7c662daadab080ac1b171d979d0d3276372
Justin <justws95@gmail.com>
2021-03-17 01:52:18 -0400

    Roughly draft of init command

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,4 +1,4 @@
-const {Command} = require('@oclif/command');
+const {Command, flags} = require('@oclif/command');
 const yaml = require('js-yaml');
 const fs = require('fs');
 const os = require('os');
@@ -10,10 +10,14 @@ const brunoUtils = require('./../common/utils');
 
 class InitCommand extends Command {
   async run() {
+    const {flags} = this.parse(InitCommand);
+
     // Verify this is not an existing Bruno project
-    if (brunoUtils.is_bruno_repo()) {
-      console.log(chalk.redBright('Error: This is already a bruno tracked repository'));
-      return;
+    if (!flags.force) {
+      if (brunoUtils.is_bruno_repo()) {
+        console.log(chalk.redBright('Error: This is already a bruno tracked repository'));
+        return;
+      }
     }
 
     // Display the welcome banner
@@ -46,7 +50,11 @@ class InitCommand extends Command {
         documentation_dir: 'docs',
         libs_dir: 'libs',
         extras_dir: 'extras',
-      }
+      },
+      compiler_flags: {
+        debug: [],
+        production: [],
+      },
     };
 
     switch (os.platform()) {
@@ -70,7 +78,7 @@ class InitCommand extends Command {
 
     const questions = [
       {
-        name: 'project-name',
+        name: 'project_name',
         type: 'input',
         default: BRUNO_PROJ.project_name,
         message: 'Name of new Bruno project: ',
@@ -82,13 +90,13 @@ class InitCommand extends Command {
         message: 'Compiler: ',
       },
       {
-        name: 'build-system',
+        name: 'build_system',
         type: 'input',
         default: BRUNO_PROJ.build_system,
         message: 'Build System: ',
       },
       {
-        name: 'lang-std',
+        name: 'language_standard',
         type: 'list',
         choices: [
           'Latest',
@@ -110,24 +118,70 @@ class InitCommand extends Command {
     ];
 
     inquirer.prompt(questions)
+    .then((answers, inquirer) => {
+      if (!answers.pitchfork) {
+        // TODO: Find a more elegant way to do this
+        delete BRUNO_PROJ.project_structure.build_dir;
+        delete BRUNO_PROJ.project_structure.data_dir;
+        delete BRUNO_PROJ.project_structure.documentation_dir;
+        delete BRUNO_PROJ.project_structure.examples_dir;
+        delete BRUNO_PROJ.project_structure.external_dir;
+        delete BRUNO_PROJ.project_structure.extras_dir;
+        delete BRUNO_PROJ.project_structure.libs_dir;
+        delete BRUNO_PROJ.project_structure.test_dir;
+        delete BRUNO_PROJ.project_structure.tools_dir;
+
+        const structure_options = [
+          {
+            name: 'src-dir',
+            type: 'input',
+            default: '.',
+            message: 'Source file(s) directory: ',
+          },
+          {
+            name: 'include-dir',
+            type: 'input',
+            default: '.',
+            message: 'Include file(s) directory: ',
+          },
+        ];
+
+        inquirer.prompt(structure_options)
+        .then(answers => {
+          BRUNO_PROJ.project_structure.source_dir = answers['src-dir'];
+          BRUNO_PROJ.project_structure.include_dir = answers['include-dir'];
+        });
+      }
+
+      return answers;
+    })
     .then(answers => {
-      if (answers.p)
+      for (const key in answers) {
+        if (key === 'pitchfork') {
+          continue;
+        } else {
+          BRUNO_PROJ[key] = answers[key];
+        }
+      }
     })
     .catch(error => {
-      if(error.isTtyError) {
+      if (error.isTtyError) {
         // Prompt couldn't be rendered in the current environment
       } else {
         // Something else went wrong
       }
     })
-    .finally(answers => {
-      let yamlStr = yaml.safeDump(BRUNO_PROJ);
-      fs.writeFileSync('./bruno.yml', yamlStr, 'utf8');
-      console.log(chalk.magenta(`Initialize new Bruno project at ${BRUNO_PATH}`));
+    .finally(() => {
+      fs.writeFileSync('./bruno.yml', yaml.safeDump(BRUNO_PROJ), 'utf8');
+      console.log(chalk.magenta(`Initialized new Bruno project at ${BRUNO_PATH}`));
     });
   }
 }
 
 InitCommand.description = 'Initialize a new Bruno project.';
+
+InitCommand.flags = {
+  force: flags.boolean({char: 'f', description: 'Overwrite existing bruno file.'}),
+};
 
 module.exports = InitCommand;
