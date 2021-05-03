091daf404fbf761d2eea4e8a8e125394c34f64d2
Chandler Wilder <cwilder8@vols.utk.edu>
2021-05-02 23:34:44 -0400

    Gets yml filenames for parsing

--- src/commands/audit.js
+++ src/commands/audit.js
@@ -4,7 +4,31 @@ class AuditCommand extends Command {
   async run() {
     const {flags} = this.parse(AuditCommand)
     const name = flags.name || 'world'
-    this.log(`hello ${name} from /home/justin/School/SeniorDesign/Bruno-CLI/bruno-cli/src/commands/audit.js`)
+    this.log(`hello ${name}`)
+    const fs = require('fs');
+    const yaml = require('js-yaml');
+    var ymlfiles = new Array();
+
+    let dirname = '../bin';
+    console.log("Going to get file info!");
+    let filenames = fs.readdirSync(dirname);
+    
+    console.log("\nFilenames in directory:");
+    filenames.forEach((file) => {
+      if (file.split('.').pop() == 'yml') {
+        ymlfiles.push(file);
+      }
+    });
+    
+    try {
+      let fullpath = '../bin/' + ymlfiles[0];
+      let fileContents = fs.readFileSync(fullpath, 'utf8');
+      data = yaml.safeLoad(fileContents);
+
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
 
